2b6eab03f44f48ea8fa67e79def16f867eb83466
Misan Etchie <56762634+MisanEtchie@users.noreply.github.com>
2026-02-03 10:08:57 -0700

    broken calc test funct

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,21 @@
 import unittest
-from src.calculator import add, subtract, divide 
+from src.calculator import add, subtract, divide
+
 class TestCalculator(unittest.TestCase):
    def test_add(self):
-    self.assertEqual(add(2, 3), 5)
-    self.assertEqual(add(-1, 1), 0)
-    self.assertEqual(add(-1, -1), -2)
-def test_subtract(self):
-  # COMPLETE HERE
-def test_divide(self):
-  # COMPLETE HERE
+       self.assertEqual(add(2, 3), 5)
+       self.assertEqual(add(-1, 1), 0)
+       self.assertEqual(add(-1, -1), -2)
+
+   def test_subtract(self):
+       self.assertEqual(subtract(10, 3), 7)
+       self.assertEqual(subtract(-1, 1), -2)
+       self.assertEqual(subtract(-5, -2), -3)
+
+   def test_divide(self):
+       self.assertEqual(divide(10, 2), 5)
+       self.assertEqual(divide(-9, 3), -3)
+       self.assertAlmostEqual(divide(1, 3), 1/3)
+
 if __name__ == '__main__':
    unittest.main()
